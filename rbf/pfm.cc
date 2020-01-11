7324170aa33fffbc55b549bd0ff59bcfba57a0b1
yundai424 <yundai424@gmail.com>
2020-01-11 13:00:47 +0800

    miscellaneous

--- rbf/pfm.cc
+++ rbf/pfm.cc
@@ -1,14 +1,7 @@
-#include "pfm.h"
 #include <iostream>
 
-bool ifFileExists(const std::string &fileName) {
-    std::ifstream file(fileName);
-    if (file) {
-        file.close();
-        return true;
-    }
-    return false;
-}
+#include "pfm.h"
+#include "logger.h"
 
 /**
  * ======= PagedFileManager =======
@@ -16,37 +9,37 @@ bool ifFileExists(const std::string &fileName) {
 PagedFileManager *PagedFileManager::_pf_manager = nullptr;
 
 PagedFileManager &PagedFileManager::instance() {
-    static PagedFileManager _pf_manager = PagedFileManager();
-    return _pf_manager;
+  static PagedFileManager _pf_manager = PagedFileManager();
+  return _pf_manager;
 }
 
 PagedFileManager::PagedFileManager() = default;
 
-PagedFileManager::~PagedFileManager() {delete _pf_manager;}
+PagedFileManager::~PagedFileManager() { delete _pf_manager; }
 
 PagedFileManager::PagedFileManager(const PagedFileManager &) = default;
 
 PagedFileManager &PagedFileManager::operator=(const PagedFileManager &) = default;
 
 RC PagedFileManager::createFile(const std::string &fileName) {
-    FileHandle handler;
-    return handler.createFile(fileName);
+  FileHandle handler;
+  return handler.createFile(fileName);
 }
 
 RC PagedFileManager::destroyFile(const std::string &fileName) {
-    if (!ifFileExists(fileName))
-        return -1;
-    if (remove(fileName.c_str()) != 0)
-        return -1;
-    return 0;
+  if (!ifFileExists(fileName)) {
+    DB_WARNING << "try to delete non-exist file " << fileName;
+    return -1;
+  }
+  return remove(fileName.c_str());
 }
 
 RC PagedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
-    return fileHandle.openFile(fileName);
+  return fileHandle.openFile(fileName);
 }
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle) {
-    return fileHandle.closeFile();
+  return fileHandle.closeFile();
 }
 
 /**
@@ -54,96 +47,117 @@ RC PagedFileManager::closeFile(FileHandle &fileHandle) {
  */
 
 FileHandle::FileHandle() {
-    readPageCounter = 0;
-    writePageCounter = 0;
-    appendPageCounter = 0;
+  readPageCounter = 0;
+  writePageCounter = 0;
+  appendPageCounter = 0;
 }
 
 FileHandle::~FileHandle() = default;
 
-std::fstream & FileHandle::getFile() {return _file;}
+RC FileHandle::openFile(const std::string &fileName) {
+  if (!PagedFileManager::ifFileExists(fileName)) {
+    DB_WARNING << "try to open non-exist file " << fileName;
+    return -1;
+  }
 
-RC FileHandle::openFile(const std::string & fileName) {
-    if (!ifFileExists(fileName))
-        return -1;
-    if (_file.is_open())
-        return -1;
-    _file.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
+  if (_file.is_open()) {
+    DB_WARNING << "file " << name << "already open!";
+    return -1;
+  }
 
-    // update counter using metadata
-    _file.seekg(0);
-    _file.read((char*)&readPageCounter, sizeof(unsigned));
-    _file.read((char*)&writePageCounter, sizeof(unsigned));
-    _file.read((char*)&appendPageCounter, sizeof(unsigned));
-    return 0;
+  _file.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
+  if (!_file.good()) {
+    DB_WARNING << "failed to open file " << fileName;
+    return -1;
+  }
+  name = fileName;
+  // update counter using metadata
+  _file.seekg(0);
+  _file.read((char *) &readPageCounter, sizeof(unsigned));
+  _file.read((char *) &writePageCounter, sizeof(unsigned));
+  _file.read((char *) &appendPageCounter, sizeof(unsigned));
+//    DB_DEBUG << "header read. readPageCounter: " << readPageCounter
+//             << " writePageCounter: " << writePageCounter
+//             << " appendPageCounter: " << appendPageCounter;
+  return 0;
 }
 
 RC FileHandle::closeFile() {
-    if (!_file.is_open())
-        return -1;
-    // flush new counters to metadata
-    updateCounterToFile();
-    _file.close();
-    return 0;
+  if (!_file.is_open()) {
+    DB_WARNING << "File not opened.";
+    return -1;
+  }
+
+  // flush new counters to metadata
+  updateCounterToFile();
+  _file.close();
+  return 0;
 }
 
 RC FileHandle::updateCounterToFile() {
-    if (!_file.is_open())
-        return -1;
-    _file.seekp(0);
-    _file.write((char*)&readPageCounter, sizeof(unsigned));
-    _file.write((char*)&writePageCounter, sizeof(unsigned));
-    _file.write((char*)&appendPageCounter, sizeof(unsigned));
-    return 0;
+  if (!_file.is_open())
+    return -1;
+  _file.seekp(0);
+  _file.write((char *) &readPageCounter, sizeof(unsigned));
+  _file.write((char *) &writePageCounter, sizeof(unsigned));
+  _file.write((char *) &appendPageCounter, sizeof(unsigned));
+  return 0;
 }
 
-RC FileHandle::createFile(const std::string & fileName) {
-    if (ifFileExists(fileName))
-        return -1;
-    if (_file.is_open())
-        return -1;
-    _file.open(fileName, std::ios::out | std::ios::binary);
-    // write counters as metadata to head of file
-    if (updateCounterToFile() != 0)
-        return -1;
-    return closeFile();
+RC FileHandle::createFile(const std::string &fileName) {
+
+  if (PagedFileManager::ifFileExists(fileName)) {
+    DB_WARNING << "File " << fileName << " exist!";
+    return -1;
+  }
+
+  if (_file.is_open())
+    return -1;
+
+  _file.open(fileName, std::ios::out | std::ios::binary);
+  if (!_file.good()) {
+    DB_WARNING << "failed to create file " << fileName;
+    return -1;
+  }
+  // write counters as metadata to head of file
+  return closeFile();
 }
 
 RC FileHandle::readPage(PageNum pageNum, void *data) {
-    // pageNum exceed total number of pages
-    if (pageNum >= getNumberOfPages() || !_file.is_open())
-        return -1;
-    _file.seekg(OFFSET + PAGE_SIZE * pageNum);
-    _file.read((char*)data, PAGE_SIZE);
-    readPageCounter++;
-    return 0;
+  // pageNum exceed total number of pages
+  if (pageNum >= getNumberOfPages() || !_file.is_open())
+    return -1;
+  _file.seekg(GetPos(pageNum));
+  _file.read((char *) data, PAGE_SIZE);
+  readPageCounter++;
+  return 0;
 }
 
 RC FileHandle::writePage(PageNum pageNum, const void *data) {
-    if (pageNum >= getNumberOfPages() || !_file.is_open())
-        return -1;
-    _file.seekp(OFFSET + PAGE_SIZE * pageNum);
-    _file.write((char*)data, PAGE_SIZE);
-    writePageCounter++;
-    return 0;
+  if (pageNum >= getNumberOfPages() || !_file.is_open())
+    return -1;
+  _file.seekp(GetPos(pageNum));
+  _file.write((char *) data, PAGE_SIZE);
+  writePageCounter++;
+  return 0;
 }
 
 RC FileHandle::appendPage(const void *data) {
-    if (!_file.is_open())
-        return -1;
-    _file.seekp(OFFSET + PAGE_SIZE * appendPageCounter);
-    _file.write((char*)data, PAGE_SIZE);
-    appendPageCounter++;
-    return 0;
+  if (!_file.is_open())
+    return -1;
+  _file.seekp(GetPos(appendPageCounter));
+  _file.write((char *) data, PAGE_SIZE);
+  appendPageCounter++;
+  return 0;
 }
 
 unsigned FileHandle::getNumberOfPages() {
-    return appendPageCounter;
+  return appendPageCounter;
 }
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
-    readPageCount = readPageCounter;
-    writePageCount = writePageCounter;
-    appendPageCount = appendPageCounter;
-    return 0;
+  readPageCount = readPageCounter;
+  writePageCount = writePageCounter;
+  appendPageCount = appendPageCounter;
+  return 0;
 }
--- rbf/pfm.h
+++ rbf/pfm.h
@@ -15,49 +15,65 @@ typedef int RC;
 class FileHandle;
 
 class PagedFileManager {
-public:
-    static PagedFileManager &instance();                                // Access to the _pf_manager instance
+ public:
+  static PagedFileManager &instance();                                // Access to the _pf_manager instance
 
-    RC createFile(const std::string &fileName);                         // Create a new file
-    RC destroyFile(const std::string &fileName);                        // Destroy a file
-    RC openFile(const std::string &fileName, FileHandle &fileHandle);   // Open a file
-    RC closeFile(FileHandle &fileHandle);                               // Close a file
+  RC createFile(const std::string &fileName);                         // Create a new file
+  RC destroyFile(const std::string &fileName);                        // Destroy a file
+  RC openFile(const std::string &fileName, FileHandle &fileHandle);   // Open a file
+  RC closeFile(FileHandle &fileHandle);                               // Close a file
 
-protected:
-    PagedFileManager();                                                 // Prevent construction
-    ~PagedFileManager();                                                // Prevent unwanted destruction
-    PagedFileManager(const PagedFileManager &);                         // Prevent construction by copying
-    PagedFileManager &operator=(const PagedFileManager &);              // Prevent assignment
 
-private:
-    static PagedFileManager *_pf_manager;
+  static inline bool ifFileExists(const std::string &fileName) {
+    std::ifstream ifs(fileName);
+    return ifs.good();
+  }
+
+ protected:
+  PagedFileManager();                                                 // Prevent construction
+  ~PagedFileManager();                                                // Prevent unwanted destruction
+  PagedFileManager(const PagedFileManager &);                         // Prevent construction by copying
+  PagedFileManager &operator=(const PagedFileManager &);              // Prevent assignment
+
+ private:
+  static PagedFileManager *_pf_manager;
+
 };
 
 class FileHandle {
-public:
-    // variables to keep the counter for each operation
-    unsigned readPageCounter;
-    unsigned writePageCounter;
-    unsigned appendPageCounter;
+ private:
 
-    FileHandle();                                                       // Default constructor
-    ~FileHandle();                                                      // Destructor
+  inline size_t GetPos(PageNum pageNum) const {
+    return OFFSET + pageNum * PAGE_SIZE;
+  }
 
-    RC readPage(PageNum pageNum, void *data);                           // Get a specific page
-    RC writePage(PageNum pageNum, const void *data);                    // Write a specific page
-    RC appendPage(const void *data);                                    // Append a specific page
-    unsigned getNumberOfPages();                                        // Get the number of pages in the file
-    RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount,
-                            unsigned &appendPageCount);                 // Put current counter values into variables
-    std::fstream& getFile();
-    void initFile(const std::string &fileName);
-    RC openFile(const std::string &fileName);
-    RC closeFile();
-    RC createFile(const std::string &fileName);
-    RC updateCounterToFile();
+ public:
+  // variables to keep the counter for each operation
+  unsigned readPageCounter;
+  unsigned writePageCounter;
+  unsigned appendPageCounter;
 
-private:
-    std::fstream _file;
+  std::string name;
+
+  FileHandle();                                                       // Default constructor
+  ~FileHandle();                                                      // Destructor
+
+  RC readPage(PageNum pageNum, void *data);                           // Get a specific page
+  RC writePage(PageNum pageNum, const void *data);                    // Write a specific page
+  RC appendPage(const void *data);                                    // Append a specific page
+  unsigned getNumberOfPages();                                        // Get the number of pages in the file
+  RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount,
+                          unsigned &appendPageCount);                 // Put current counter values into variables
+
+  RC createFile(const std::string &fileName);
+  RC openFile(const std::string &fileName);
+  RC closeFile();
+  RC updateCounterToFile();
+
+ private:
+//  static constexpr size_t OFFSET = 12;
+
+  std::fstream _file;
 };
 
 #endif
