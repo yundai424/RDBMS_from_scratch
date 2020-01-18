e58db9dbaebd84d25ea274ceef5d835bdaff1f0b
yundai424@gmail.com <yundai424@gmail.com>
2020-01-18 13:27:58 +0800

    add logger back

--- rbf/pfm.h
+++ rbf/pfm.h
@@ -3,6 +3,134 @@
 
 #include <string>
 #include <fstream>
+#include <utility>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <vector>
+#include <ostream>
+#include <stdexcept>
+
+/******************************************
+ *
+ * =========== Logger class ============
+ * since we can not create extra files, only put it here:(
+ *
+*****************************************/
+#define MUTE_LOG
+
+enum LogLevel {
+  DEBUGGING = 0,
+  INFO = 1,
+  WARNING = 2,
+  ERROR = 3,
+  QUIET = 4
+
+};
+static std::map<LogLevel, std::string> kPrefixMap = {
+    {DEBUGGING, "\e[1;96m[TRACE]"},
+    {INFO, "\e[1;32m[INFO]"},
+    {WARNING, "\e[1;33m[WARN]"},
+    {ERROR, "\e[1;31m[ERROR]"},
+};
+static std::string kPostfix = "\e[0m";
+
+class Logger {
+ private:
+  LogLevel level_;
+  std::string file_path_;
+  std::string func_path_;
+  int line_num_;
+  bool opened_;
+  static LogLevel global_level;
+
+  inline static std::string get_file_name(const std::string &path) {
+    return path.substr(path.find_last_of("/\\") + 1);
+  }
+
+ public:
+  Logger() = delete;
+
+  Logger(const Logger &) = delete;
+
+  explicit Logger(LogLevel level, std::string file_path, int line_num, std::string func_path)
+      : level_(level), file_path_(move(file_path)), line_num_(line_num), func_path_(move(func_path)),
+        opened_(false) {
+    std::cout << std::boolalpha;
+  }
+
+  ~Logger() {
+    if (opened_) std::cout << std::endl;
+  }
+
+  template<typename T>
+  Logger &operator<<(const T &v) {
+    if (level_ < global_level) return *this;
+    if (!opened_) {
+      file_path_ = get_file_name(file_path_);
+      std::cout << std::setw(14) << std::left << std::dec << kPrefixMap[level_]
+           << " In '" << func_path_ << "' " << file_path_ << ":" << line_num_
+           << " " << kPostfix;
+      opened_ = true;
+    }
+    std::cout << v;
+    return *this;
+  }
+
+  static void SetGlobalLogLevel(LogLevel level) { global_level = level; }
+
+  static LogLevel GetGlobalLogLevel() { return global_level; }
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+  os << "[";
+  for (int i = 0; i < v.size(); ++i) {
+    os << v[i];
+    if (i != v.size() - 1) os << ", ";
+  }
+  os << "]\n";
+  return os;
+}
+
+#define DB_DEBUG Logger(LogLevel::DEBUGGING, __FILE__, __LINE__, __FUNCTION__)
+#define DB_INFO Logger(::LogLevel::INFO, __FILE__, __LINE__, __FUNCTION__)
+#define DB_WARNING Logger(::LogLevel::WARNING, __FILE__, __LINE__, __FUNCTION__)
+#define DB_ERROR Logger(::LogLevel::ERROR, __FILE__, __LINE__, __FUNCTION__)
+
+static std::string print_bytes(const void *ptr, int size) {
+  std::ostringstream oss;
+  const unsigned char *p = (const unsigned char *) ptr;
+  int i;
+  char buf[4] = {0};
+  for (i = 0; i < size; i++) {
+    sprintf(buf, "%02hhX ", p[i]);
+    oss << buf;
+  }
+  return oss.str();
+}
+
+static std::string print_bits(const void *ptr, int size) {
+  std::ostringstream oss;
+  const unsigned char *p = (const unsigned char *) ptr;
+  int i;
+  for (i = 0; i < size; i++) {
+    for (int j = 0; i < 8; ++j) {
+      unsigned char mask = 1 << (7 - j);
+      if (mask & p[i]) std::cout << 1;
+      else std::cout << 0;
+    }
+    oss << ' ';
+  }
+  return oss.str();
+}
+
+/******************************************
+ *
+ * =========== Logger class ============
+ *
+*****************************************/
 
 typedef unsigned PageNum;
 typedef int RC;
