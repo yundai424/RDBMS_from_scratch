0cfbaee0104ed85399dbc56b76a7c656066f1ffa
yundai424 <yundai424@gmail.com>
2020-02-05 13:49:22 -0800

    gradescope cannot create catalog

--- rm/rm.h
+++ rm/rm.h
@@ -136,7 +136,7 @@ bool RelationManager::ifTableExists(const std::string &tableName) {
 }
 
 std::string RelationManager::getTableFileName(const std::string &tableName, bool is_system_table) {
-  return is_system_table ? tableName + ".ctlg" : tableName;
+  return is_system_table ? tableName + ".catalog" : tableName;
 }
 
 void RelationManager::loadDbIfExist() {
