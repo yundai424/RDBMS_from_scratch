dfe0282a433ecccfb9d0b361a9a3e9eea4d7cbd3
yundai424 <yundai424@gmail.com>
2020-02-26 13:24:11 -0800

    trim Key::toString

--- ix/ix.cc
+++ ix/ix.cc
@@ -465,7 +465,7 @@ std::string Key::toString(bool key_val_only) const {
       break;
     case AttrType::TypeReal:val_str = std::to_string(f);
       break;
-    case AttrType::TypeVarChar: val_str = s;
+    case AttrType::TypeVarChar: val_str = s.size() > 15 ? s.substr(15) : s;
       break;
   }
   if (key_val_only) return val_str;
